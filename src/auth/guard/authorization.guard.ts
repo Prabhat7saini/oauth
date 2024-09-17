43eceee3a80ca3dba0c50d94812aa0925096d741
Prabhat Saini <Prabhat.saini@binmile.com>
2024-09-17 10:49:01 +0530

    add

--- src/auth/guard/authorization.guard.ts
+++ src/auth/guard/authorization.guard.ts
@@ -9,13 +9,9 @@ export class AuthorizationGuard implements CanActivate {
     constructor(private readonly reflection: Reflector) { }
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         try {
-            console.log(` inside the authorizationGuard `)
             const request = context.switchToHttp().getRequest();
-            console.log(`request: ${request.user.role}`)
             const requiredRole = this.reflection.get(ROLES_KEY, context.getHandler());
-            console.log(`this is a required role ${requiredRole}`);
-            console.log(`this is the user role ${request.user.role}`)
-
+            
             if (requiredRole !== request.user.role) {
                 throw new ForbiddenException(ERROR_MESSAGES.PROTECTED_ROUTE);
             }
